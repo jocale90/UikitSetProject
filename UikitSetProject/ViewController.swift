f65f6506a4c5c9f7960cac06dcb2f8ed0b5b3c63
Jose Pernia <joseapc90@gmail.com>
2024-05-14 18:07:59 -0400

    Initial commit

---
+++ UikitSetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UikitSetProject
+//
+//  Created by Jose Pernia on 2024-05-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
